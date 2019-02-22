b300ef7d21366614e5bc6d5d0eb2c9164c253343
Josh <joshrlear@gmail.com>
2019-02-22 09:41:22 -0800

    created html template, css styling andsome js notes for function

---
+++ palPer.js
@@ -0,0 +1,7 @@
+// Get form
+const myForm = document.getElementById(myForm);
+
+// Get input text > loop through text > create object-
+// storing any letters with frequency that is odd and-
+// deleting any that are even > 'if' only one odd letter-
+// left in object then return true, else false.
