fd3057cabf26eb4d2f2e26ef40a2a59ce51a6875
Josh <joshrlear@gmail.com>
2019-02-22 17:36:41 -0800

    fixed evaluate function

--- palPer.js
+++ palPer.js
@@ -1,25 +1,30 @@
-// Get form
-const myForm = document.getElementById(myForm);
-
 // create function that accepts input text as parameter >
-function evaluate(word){
+function evaluate(txt){
 
     // create object to store values
     const obj = {};
 
-    // get input text
-    let word = document.getElementById(word);
-
     // loop through word
-    for(let i=0; i <= word.lengthl; i++){
+    for(let i=0; i < txt.length; i++){
 
         // if i exists (i.value = 1) then delete the value
         // if i doen't exist then assign value i = 1
-        obj[word[i]] ? delete obj[word[i]]
-        : obj[word[i]] = 1;
+        if(obj[txt[i]]) {
+            delete obj[txt[i]];
+        }
+        else {
+            obj[txt[i]] = 1;
+        }
     }
+  
+
     // if object has two or more values, return false.
     // if 1 or less values, return true 
-    (obj.length = 2) ? false : true;
-    return 
+    const keys = Object.keys(obj);
+    if (keys.length < 2) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
