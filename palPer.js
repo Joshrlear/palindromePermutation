d80b6f2ba6193792660b5aee3510cb419abe55a7
Josh <joshrlear@gmail.com>
2019-02-22 17:35:56 -0800

    added evaluate function to js file

--- palPer.js
+++ palPer.js
@@ -1,7 +1,25 @@
 // Get form
 const myForm = document.getElementById(myForm);
 
-// Get input text > loop through text > create object-
-// storing any letters with frequency that is odd and-
-// deleting any that are even > 'if' only one odd letter-
-// left in object then return true, else false.
+// create function that accepts input text as parameter >
+function evaluate(word){
+
+    // create object to store values
+    const obj = {};
+
+    // get input text
+    let word = document.getElementById(word);
+
+    // loop through word
+    for(let i=0; i <= word.lengthl; i++){
+
+        // if i exists (i.value = 1) then delete the value
+        // if i doen't exist then assign value i = 1
+        obj[word[i]] ? delete obj[word[i]]
+        : obj[word[i]] = 1;
+    }
+    // if object has two or more values, return false.
+    // if 1 or less values, return true 
+    (obj.length = 2) ? false : true;
+    return 
+}
